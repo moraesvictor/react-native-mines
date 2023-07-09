d62afba958cbf1e9bc754441ce6fb8069e0f2c75
moraesvictor <victor.araujo@digitalpages.com.br>
2023-07-08 21:29:40 -0300

    feat: create logical rules

--- src/useApp.js
+++ src/useApp.js
@@ -42,3 +42,65 @@ export const createMinedBoard = (rows, columns, minesAmount) => {
   spreadMine(board, minesAmount);
   return board;
 };
+
+export const cloneBoard = board => {
+  return board.map(rows => {
+    return rows.map(field => {
+      return {...field};
+    });
+  });
+};
+
+export const getNeighbors = (board, row, column) => {
+  const neighbors = [];
+  const rows = [row - 1, row, row + 1];
+  const columns = [column - 1, column, column + 1];
+  rows.forEach(r => {
+    columns.forEach(c => {
+      const isDiferent = r !== row || c !== column;
+      const isValidRow = r >= 0 && r < board.length;
+      const isValidColumn = c >= 0 && c < board[0].length;
+      if (isDiferent && isValidColumn && isValidRow) {
+        neighbors.push(board[r][c]);
+      }
+    });
+  });
+  return neighbors;
+};
+
+export const safeNeighborhood = (board, row, column) => {
+  const safes = (result, neighbor) => result && !neighbor.mined;
+  return getNeighbors(board, row, column).reduce(safes, true);
+};
+
+export const openField = (board, row, column) => {
+  const field = board[row][column];
+  if (!field.opened) {
+    field.opened = true;
+    if (field.mined) {
+      field.exploded = true;
+    } else if (safeNeighborhood(board, row, column)) {
+      getNeighbors(board, row, column).forEach(n =>
+        openField(board, n.row, n.column),
+      );
+    } else {
+      const neighbors = getNeighbors(board, row, column);
+      field.nearMines = neighbors.filter(n => n.mined).length;
+    }
+  }
+};
+
+export const fields = board => [].concat(...board);
+
+export const hadExplosion = board =>
+  fields(board).filter(field => field.exploded).length > 0;
+
+export const pendding = field =>
+  (field.mined && !field.flag) || (!field.opened && !field.mined);
+
+export const wonGame = board => fields(board).filter(pendding) === 0;
+
+export const showMines = board =>
+  fields(board)
+    .filter(field => field.mined)
+    .forEach(f => (f.opened = true));
